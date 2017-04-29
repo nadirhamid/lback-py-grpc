26b32b653377e1cde9b96bd6ec374fb0eea9ca39
Nadir Hamid <nadir@ubuntu-1gb-tor1-01.localdomain>
2017-04-29 22:20:17 +0000

    Make DoBackup not dependant on master DB

--- lback_grpc/agent.py
+++ lback_grpc/agent.py
@@ -1,5 +1,5 @@
 from lback.backup import Backup
-from lback.utils import lback_backup, lback_backup_chunked_file, lback_backup_remove, lback_output
+from lback.utils import lback_backup, lback_backup_chunked_file, lback_backup_remove, lback_output, lback_backup_path
 from lback.restore import Restore
 from . import agent_pb2
 from . import agent_pb2_grpc
@@ -13,29 +13,30 @@ class Agent(agent_pb2_grpc.AgentServicer):
     lback_output("Received COMMAND DoBackup")
     request_iterator, iter_copy= tee( request_iterator )
     request = next(iter_copy)
-    db_backup = lback_backup( request.id )
+    folder= lback_backup_path( request.id )
     lback_output("Running backup on %s"%( request.id ))
-    backup = Backup( request.id, db_backup.folder )
+    backup = Backup( request.id, folder )
     def backup_chunked_iterator():
-    	for backup_cmd_chunk in request_iterator:
-	     yield backup_cmd_chunk.raw_data
+        for backup_cmd_chunk in request_iterator:
+         yield backup_cmd_chunk.raw_data
     try:
-	backup.write_chunked( backup_chunked_iterator() )
+        backup.write_chunked( backup_chunked_iterator() )
     except Exception,ex:
-	print_exc(ex)
-	return shared_pb2.BackupCmdStatus( errored=True )
+        print_exc(ex)
+        return shared_pb2.BackupCmdStatus( errored=True )
+
     lback_output("BACKUP COMPLETE")
     return shared_pb2.BackupCmdStatus( errored=False )
 
   def DoRelocateTake(self, request, context):
-     lback_output("Received COMMAND DoRelocateTake")
-     try:
+    lback_output("Received COMMAND DoRelocateTake")
+    try:
         iterator = lback_backup_file_chunks( request.id )
-	for file_chunk_res in iterator:
-    	    lback_output("PACKING RELOCATE BACKUP CHUNK")
-	    yield shared_pb2.RelocateCmdGiveStatus( raw_data=file_chunk_res, errored=False)
-     except Exception,ex:
-         yield shared_pb2.RelocateCmdGiveStatus( errored=True )
+        for file_chunk_res in iterator:
+            lback_output("PACKING RELOCATE BACKUP CHUNK")
+            yield shared_pb2.RelocateCmdGiveStatus( raw_data=file_chunk_res, errored=False)
+    except Exception,ex:
+        yield shared_pb2.RelocateCmdGiveStatus( errored=True )
   def DoRelocateGive(self, request_iterator, context):
     lback_output("Received COMMAND DoRelocateGive")
     request_iterator, iter_copy = tee( request_iterator )
@@ -43,31 +44,31 @@ class Agent(agent_pb2_grpc.AgentServicer):
     db_backup =lback_backup( request.id )
     backup = Backup( request.id, db_backup.folder )
     def relocate_cmd_chunked_iterator():
-	 for relocate_cmd_chunk in request_iterator:
-    	      lback_output("SAVING RELOCATE BACKUP GIVE CHUNK")
-	      yield relocate_cmd_chunk.raw_data
+        for relocate_cmd_chunk in request_iterator:
+            lback_output("SAVING RELOCATE BACKUP GIVE CHUNK")
+            yield relocate_cmd_chunk.raw_data
     try:
-	iterator = backup.run_chunked( relocate_chunked_iterator )
+        iterator = backup.run_chunked( relocate_chunked_iterator )
         for relocate_chunk_res in iterator:
-	    yield shared_pb2.RelocateCmdStatus(errored=False)
+            yield shared_pb2.RelocateCmdStatus(errored=False)
     except Exception,ex:
-	 print_exc(ex)
-	 yield shared_pb2.RelocateCmdStatus(errored=True )
+        print_exc(ex)
+        yield shared_pb2.RelocateCmdStatus(errored=True )
 
   def DoRestore(self, request, context):
     lback_output("Received COMMAND DoRestore")
     db_backup =lback_backup( request.id )
     restore = Restore( request.id, folder=db_backup.folder )
     try:
-	 iterator = restore.read_chunked()
-         for restore_file_chunk in iterator:
-    	     lback_output("PACKING RESTORE CHUNK")
-	     yield shared_pb2.RestoreCmdStatus( 
-		errored=False,
-		raw_data=restore_file_chunk )
+        iterator = restore.read_chunked()
+        for restore_file_chunk in iterator:
+            lback_output("PACKING RESTORE CHUNK")
+            yield shared_pb2.RestoreCmdStatus( 
+                errored=False,
+               raw_data=restore_file_chunk )
     except Exception,ex:
-	print_exc(ex)
-	yield shared_pb2.RestoreCmdStatus( errored=True )
+        print_exc(ex)
+        yield shared_pb2.RestoreCmdStatus( errored=True )
 
   def DoRm(self, request, context):
     lback_output("Received COMMAND DoRm")
